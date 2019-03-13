30aeab3d0d3f683d9bfc14eda3836282f086b398
[trapintrovert] <[mcneo062@gmail.com]>
2019-03-13 18:11:53 +0100

    stage

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,6 @@ gulp.task('serve', ['sass'], function() {
         server: "./src"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
